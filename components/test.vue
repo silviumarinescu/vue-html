0341b1782b12849b9fb65af5a052af7da4958c6c
Silviu Marinescu <f.silviumarinescu@gmail.com>
2020-10-05 22:38:04 +0300

    wip

---
+++ components/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>test</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: { a: "b" },
+    };
+  },
+  methods: {
+    test() {},
+  },
+};
+</script>
